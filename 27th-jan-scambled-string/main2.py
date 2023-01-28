7ab9914b64ad695a5fcdfdd08f60aa869cde1733
Anupam <anupam@Anupams-MBP.lan>
2023-01-27 22:51:58 -0500

    commit

--- 27th-jan-scambled-string/main2.py
+++ 27th-jan-scambled-string/main2.py
@@ -1,8 +1,17 @@
+#User function Template for python3
 from collections import Counter
-
-print(Counter(['o']) == Counter(['i']))
-print(Counter(['i']))
-# a = "ocder"
-# print(len(a))
-# print(a[:1])
-# print(a[-1:])
+from functools import lru_cache
+class Solution:
+    @lru_cache(None)
+    def isScramble(self,S1: str, S2: str):
+        if  Counter(S1) != Counter(S2): 
+            return False
+        if len(S1) == 1: 
+            return True
+        for i in range(1,len(S1)): 
+            if(self.isScramble(S1[:i],S2[:i]) and self.isScramble(S1[i:],S2[i:])): 
+                return True
+            if(self.isScramble(S1[:i],S2[-i:]) and self.isScramble(S1[i:],S2[:-i])): 
+                return True
+        return False
+        ##code here
